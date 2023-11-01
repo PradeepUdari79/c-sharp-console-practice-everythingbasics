b013db9d8d6ec8e97a16bad1893ff82fae132469
Dell <Dell@DESKTOP-GRGIP8E>
2023-11-01 12:52:05 +0530

    Add project files.

---
+++ c sharp console practice everythingbasics/Program.cs
@@ -0,0 +1,42 @@
+﻿
+
+using System;
+using System.Linq;
+
+namespace c_sharp_console_practice_everythingbasics
+{
+    class Program
+    {
+     
+            
+        static void Main(string[] args)
+        {
+
+
+
+            // string name = "my favourite lover is god ";
+
+            //string  result = name.Replace("favourite", "beautiful");
+            // Console.WriteLine(result);
+
+            int[] numbrs = { 1, 2, 4, 5, 8, 9 } ;
+            var lownumbers = from n in numbrs where (n >4 && n<8) select n;
+            foreach(var x in lownumbers)
+            {
+                Console.WriteLine(x);
+
+            }
+            Console.ReadLine();
+
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
